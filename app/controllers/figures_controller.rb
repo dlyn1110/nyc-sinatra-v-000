1178e7c6f542b619d7eb8495e2d575fd2ede98f3
Dani Lyn <dlyn1110@gmail.com>
2019-01-05 22:07:20 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,6 +2,20 @@ class FiguresController < ApplicationController
   # add controller methods
 
   get '/figures' do
-    erb :'figures/index' 
+   @figures = Figure.all
+   erb :'/figures/index'
+ end
+
+ get '/figures/new' do
+   erb :'figures/new'
+ end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb:'/figures/show'
+  end
+
+  get '/figures/' do
+    binding.pry
   end
 end
